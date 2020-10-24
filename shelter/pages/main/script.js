d343d49cab881889f498480ee216ee78c227841f
ortophius@mail.ru <ortophius@mail.ru>
2020-10-24 23:20:19 +0400

    feat: implement mobile menu

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -3,3 +3,23 @@ for (let i = 0; i < photos.length; i++) {
   const photo = photos[i];
   photo.style.backgroundImage = `url(${photo.getAttribute('data-src')})`;
 }
+
+const burger = document.querySelector('.menu__burger'),
+  menu = document.querySelector('.menu');
+
+let menuActive = false;
+
+function toggleMenu() {
+  menuActive = !menuActive;
+
+  if(menuActive) {
+    menu.classList.add('menu_active');
+    burger.classList.add('menu__burger_active');
+  }
+  else {
+    menu.classList.remove('menu_active');
+    burger.classList.remove('menu__burger_active');
+  }
+}
+
+burger.addEventListener('click', toggleMenu);
--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -3,3 +3,26 @@ for (let i = 0; i < photos.length; i++) {
   const photo = photos[i];
   photo.style.backgroundImage = `url(${photo.getAttribute('data-src')})`;
 }
+
+const burger = document.querySelector('.menu__burger'),
+  menu = document.querySelector('.menu'),
+  logo = document.querySelector('.logo');
+
+let menuActive = false;
+
+function toggleMenu() {
+  menuActive = !menuActive;
+
+  if(menuActive) {
+    menu.classList.add('menu_active');
+    burger.classList.add('menu__burger_active');
+    logo.classList.add('logo_active');
+  }
+  else {
+    menu.classList.remove('menu_active');
+    burger.classList.remove('menu__burger_active');
+    logo.classList.remove('logo_active');
+  }
+}
+
+burger.addEventListener('click', toggleMenu);
