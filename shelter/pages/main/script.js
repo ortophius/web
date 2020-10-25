60b798f582466b277e525b6d232397b0b34e675e
ortophius@mail.ru <ortophius@mail.ru>
2020-10-25 19:31:48 +0400

    fix: correct mobile menu behavior

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -5,21 +5,37 @@ for (let i = 0; i < photos.length; i++) {
 }
 
 const burger = document.querySelector('.menu__burger'),
-  menu = document.querySelector('.menu');
-
-let menuActive = false;
-
-function toggleMenu() {
-  menuActive = !menuActive;
-
-  if(menuActive) {
+  menu = document.querySelector('.menu'),
+  blackout = document.querySelector('.blackout'),
+  logo = document.querySelector('.logo');
+  
+  let menuActive = false;
+  
+  function toggleMenu() {
+    menuActive = !menuActive;
+  
+    if(menuActive) openMenu();
+    else closeMenu()
+  }
+  
+  function openMenu() {
+    menuActive = true;
+  
     menu.classList.add('menu_active');
     burger.classList.add('menu__burger_active');
+    blackout.classList.add('blackout_active');
+    logo.classList.add('logo_active');
+    blackout.addEventListener('click', closeMenu);
   }
-  else {
+  
+  function closeMenu() {
+    menuActive = false;
+  
     menu.classList.remove('menu_active');
     burger.classList.remove('menu__burger_active');
+    blackout.classList.remove('blackout_active');
+    logo.classList.remove('logo_active');
+    blackout.removeEventListener('click', closeMenu);
   }
-}
-
+  
 burger.addEventListener('click', toggleMenu);
