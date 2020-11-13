189acddd5d1cc7fccb783e3ce8fc960303e1161e
ortophius@mail.ru <ortophius@mail.ru>
2020-11-14 03:46:15 +0400

    feat: main menu minimal implementation

--- gem-puzzle/src/modules/Menu/MenuItem.ts
+++ gem-puzzle/src/modules/Menu/MenuItem.ts
@@ -4,9 +4,10 @@ import Text from '../Text/Text';
 export default class MenuItem extends Text {
   active: boolean = true;
 
+  hover: boolean = false;
+
   constructor(text: string, size?: number, x?: number, y?: number) {
     super(text, size, 'Arial', x, y);
-    this.color = Config.menu.color.active;
     this.setupListeners();
   }
 
@@ -17,22 +18,29 @@ export default class MenuItem extends Text {
   }
 
   private onMouseOver() {
-    if (this.active) this.color = Config.menu.color.hover;
     Config.canvas.style.cursor = 'pointer';
+    this.hover = true;
   }
 
   private onMouseOut() {
-    this.color = (this.active)
-      ? Config.menu.color.active
-      : Config.menu.color.inactive;
-
+    this.hover = false;
     Config.canvas.style.cursor = 'default';
   }
 
   render(ctx: CanvasRenderingContext2D) {
     ctx.save();
 
-    ctx.fillStyle = this.color;
+    const colors = Config.menu.color;
+    let color: string;
+
+    if (this.active) {
+      if (this.hover) color = colors.hover;
+      else color = colors.active;
+    } else {
+      color = colors.inactive;
+    }
+
+    ctx.fillStyle = color;
     ctx.font = `${this.size}px ${this.fontFamily}`;
     ctx.textBaseline = 'top';
 
