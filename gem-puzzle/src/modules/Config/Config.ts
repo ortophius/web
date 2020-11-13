d4789357d2bf638aeb4a2512b6b81e513d5b8f16
ortophius@mail.ru <ortophius@mail.ru>
2020-11-13 20:37:11 +0400

    feat: add MouseEventDispatcher as Config property

--- gem-puzzle/src/modules/Config/Config.ts
+++ gem-puzzle/src/modules/Config/Config.ts
@@ -1,13 +1,17 @@
+import MouseEventDispatcher from '../MouseEventDispatcher/MouseEventDispatcher';
+
 interface IConfig {
   ctx: CanvasRenderingContext2D | null;
   canvas: HTMLCanvasElement | null;
   DOMElement: HTMLElement | null;
+  MouseEvents: MouseEventDispatcher | null;
 }
 
 const Config: IConfig = {
   ctx: null,
   canvas: null,
   DOMElement: null,
+  MouseEvents: null,
 };
 
 export default Config;
